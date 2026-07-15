d4e4bca0914c715c93e237cbab6f98838aa8e3a5
Divyansh Saini <sainidivyansh2006@gmail.com>
2026-07-15 16:44:41 +0000

    frontend c

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
-// import RelatedDoctors from './components/RelatedDoctors'
+import RelatedDoctors from '../components/RelatedDoctors'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 
