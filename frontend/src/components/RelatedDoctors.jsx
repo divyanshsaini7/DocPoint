a088afa0f1c302c9948e110c6b22e4ab4eb2d10c
Divyansh Saini <sainidivyansh2006@gmail.com>
2026-07-25 18:41:11 +0000

    kj

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -23,7 +23,7 @@ const RelatedDoctors = ({ speciality, docId }) => {
       <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
       <div className='w-full flex flex-wrap gap-4 pt-5 gap-y-6'>
         {relDoc.map((item, index) => (
-          <div onClick={() => { navigate(`/appointment/${item._id}`); scrollTo(0, 0) }} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 group' key={index}>
+          <div onClick={() => { navigate(`/appointment/${item._id}`); scrollTo(0, 0) }} className='border border-blue-200 rounded-xl max-w-51 overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 group' key={index}>
             <img className='bg-[#EAEFFF] group-hover:bg-primary transition-all duration-500' src={item.image} alt="" />
             <div className='p-4'>
               <div className={`flex items-center gap-2 text-sm text-center ${item.available ? 'text-green-500' : "text-gray-500"}`}>
