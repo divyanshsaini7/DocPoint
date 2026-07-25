5be87ccc2a7a347469a08204d91798a65e743d03
Divyansh Saini <sainidivyansh2006@gmail.com>
2026-07-25 16:40:40 +0000

    hover

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -12,11 +12,11 @@ const DoctorsList = () => {
 }, [aToken])
 
   return (
-    <div className='m-5 max-h-[90vh] overflow-y-scroll'>
+    <div className='m-5 max-h-[90vh] hidden-overflow-y-scroll'>
       <h1 className='text-lg font-medium'>All Doctors</h1>
       <div className='w-full flex flex-wrap gap-4 pt-5 gap-y-6'>
         {doctors.map((item, index) => (
-          <div className='border border-[#C9D8FF] rounded-xl max-w-56 overflow-hidden cursor-pointer group' key={index}>
+          <div className='border border-[#C9D8FF] rounded-xl max-w-56 overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 group' key={index}>
             <img className='bg-[#EAEFFF] group-hover:bg-primary transition-all duration-500' src={item.image} alt="" />
             <div className='p-4'>
               <p className='text-[#262626] text-lg font-medium'>{item.name}</p>
