1dc06f93932573385b05f9076706072ae693b882
Divyansh Saini <sainidivyansh2006@gmail.com>
2026-07-25 18:48:04 +0000

    pi

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -11,9 +11,9 @@ const TopDoctors = () => {
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
       <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
       <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
-      <div className='w-full flex item-start gap-4 pt-5 gap-y-6'>
+      <div className='w-full flex flex-wrap gap-4 pt-5 gap-y-6'>
         {doctors.slice(0, 10).map((item, index) => (
-          <div onClick={() => { navigate(`/appointment/${item._id}`); scrollTo(0, 0) }} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 group' key={index}>
+          <div onClick={() => { navigate(`/appointment/${item._id}`); scrollTo(0, 0) }} className='border border-blue-200 rounded-xl max-w-59 overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 group' key={index}>
             <img className='bg-[#EAEFFF] group-hover:bg-primary transition-all duration-500' src={item.image} alt="" />
             <div className='p-4'>
               <div className={`flex items-center gap-2 text-sm text-center ${item.available ? 'text-green-500' : "text-gray-500"}`}>
